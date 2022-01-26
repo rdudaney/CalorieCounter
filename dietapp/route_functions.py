458797b1a8faf990521182da107ba62ccb631a35
rdudaney <rdudaney@github.com>
2022-01-25 16:03:44 -0800

    Separated non-route functions into separate file

---
+++ dietapp/route_functions.py
@@ -0,0 +1,148 @@
+from dietapp import app, db, bcrypt
+from dietapp.models import User, Ingredients, Units, UnitType, Meals, MealIngredients
+from flask_login import login_user, current_user, logout_user, login_required
+from flask import render_template, url_for, flash, redirect, request, abort, jsonify
+
+def parse_checkboxes(form):
+    ingredient_id_list = []
+    for v in form:
+        id = str(v).split("check")[1]
+        #print (str(v) + ": " + str(form[v]) + " - " + id[1])
+        if ("check" in v) and str(form[v]) == 'on':
+            ingredient_id_list.append(id)
+            print (str(v) + ": " + str(form[v]) + " - " + id)
+
+
+    print(ingredient_id_list)
+    return ingredient_id_list
+
+
+def fcn_save_new_from_form(form, save_type):
+    if save_type == 'NewIngredient':
+        ingredient = Ingredients(brand=form.brand.data, name=form.name.data, 
+            fat=form.fat.data, carbs=form.carbs.data, protein=form.protein.data, calories=form.calories.data, 
+            user_id=current_user.id)
+
+        ingredient.serv_weight=form.serv_weight.data
+        ingredient.weight_unit_id=form.drop_weight.data
+        ingredient.serv_volume=form.serv_volume.data
+        ingredient.volume_unit_id=form.drop_volume.data
+        ingredient.serv_count=form.serv_count.data
+        ingredient.count_unit_id=form.drop_count.data
+
+        db.session.add(ingredient)
+
+
+    db.session.commit()
+
+def fcn_update_from_form(form, id, update_type):
+    if update_type == 'UpdateIngredient':
+        ingredient = Ingredients.query.get(id)
+
+        ingredient.brand=form.brand.data
+        ingredient.name=form.name.data
+        ingredient.fat=form.fat.data
+        ingredient.carbs=form.carbs.data
+        ingredient.protein=form.protein.data
+        ingredient.calories=form.calories.data 
+
+        ingredient.serv_weight=form.serv_weight.data
+        ingredient.weight_unit_id=form.drop_weight.data
+        ingredient.serv_volume=form.serv_volume.data
+        ingredient.volume_unit_id=form.drop_volume.data
+        ingredient.serv_count=form.serv_count.data
+        ingredient.count_unit_id=form.drop_count.data
+
+    db.session.commit()
+
+
+def create_ingr_list(meal):
+    ingr_list = []
+    for i,mi in enumerate(meal.meal_ingredients):
+        serving_unit = Units.query.filter_by(id=mi.unit_id).first()
+
+        if(serving_unit.unit_type_id == 1):
+            ingr_unit = Units.query.filter_by(id=mi.ingredient.weight_unit_id).first()
+            ingr_serving_amount = mi.ingredient.serv_weight
+        elif(serving_unit.unit_type_id == 2):
+            ingr_unit = Units.query.filter_by(id=mi.ingredient.volume_unit_id).first()
+            ingr_serving_amount = mi.ingredient.serv_volume
+        else:
+            ingr_unit = Units.query.filter_by(id=mi.ingredient.count_unit_id).first()
+            ingr_serving_amount = mi.ingredient.serv_count
+
+
+        d = {}
+        d["MealIngrID"] = mi.id
+        d['IngrID'] = mi.ingredient.id
+        d['IngrName'] = mi.ingredient.name
+        d['IngrBrand'] = mi.ingredient.brand
+        d['IngrFat'] = mi.ingredient.fat
+        d['IngrCarbs'] = mi.ingredient.carbs
+        d['IngrProtein'] = mi.ingredient.protein
+        d['IngrCalories'] = mi.ingredient.calories
+        d['IngrWeightAmount'] = mi.ingredient.serv_weight
+        d['IngrVolumeAmount'] = mi.ingredient.serv_volume
+        d['IngrCountAmount'] = mi.ingredient.serv_count
+        d['IngrWeightUnitID'] = mi.ingredient.weight_unit_id
+        d['IngrVolumeUnitID'] = mi.ingredient.volume_unit_id
+        d['IngrCountUnitID'] = mi.ingredient.count_unit_id
+
+        d['ServingUnit'] = serving_unit.name
+        d['ServingAmount'] = mi.serv
+        d['ServingFat'] = mi.ingredient.fat * mi.serv * serving_unit.conversion / (ingr_serving_amount * ingr_unit.conversion)
+        d['ServingCarbs'] = mi.ingredient.carbs * mi.serv * serving_unit.conversion / (ingr_serving_amount * ingr_unit.conversion)
+        d['ServingProtein'] = mi.ingredient.protein * mi.serv * serving_unit.conversion / (ingr_serving_amount * ingr_unit.conversion)
+        d['ServingCalories'] = mi.ingredient.calories * mi.serv * serving_unit.conversion / (ingr_serving_amount * ingr_unit.conversion)
+
+        ingr_list.append(d)
+
+    return ingr_list
+
+def create_unit_dict():
+    units = Units.query.all()
+    unit_dict = {}
+    for i,u in enumerate(units):
+        d = {}
+        d["id"] = u.id
+        d["Name"] = u.name
+        d["Conversion"] = u.conversion
+        d["UnitTypeID"] = u.unit_type_id
+
+        unit_dict[u.id] = d
+
+    return unit_dict
+
+
+@login_required
+def add_ingredients(ingredient_id_list, meal_id):
+    meal = Meals.query.get_or_404(meal_id)
+    if current_user != meal.author:
+        abort(403)
+    for id in ingredient_id_list:
+        ingredient = Ingredients.query.get_or_404(id)
+        if current_user != ingredient.author:
+            abort(403)
+        #TODO: Fix ingredient serving id error
+        if ingredient.weight_unit_id is not None:
+            mi_unit_id = ingredient.weight_unit_id
+        elif ingredient.volume_unit_id is not None:
+            mi_unit_id = ingredient.volume_unit_id
+        elif ingredient.count_unit_id is not None:
+            mi_unit_id = ingredient.count_unit_id
+        mi = MealIngredients(meal_id=meal_id,ingredient_id=id,serv=0,unit_id=mi_unit_id)
+        db.session.add(mi)
+    db.session.commit()
+    #TODO: Remove  return redirect
+    return redirect(url_for('update_meal',meal_id=meal_id))
+
+@login_required
+def delete_ingredients(ingredient_id_list):
+    for id in ingredient_id_list:
+        ingredient = Ingredients.query.get_or_404(id)
+        if current_user != ingredient.author:
+            abort(403)
+        ingredient.obsolete = True
+        db.session.commit()
+    flash('Your ingredient has been deleted', 'success')
+    return redirect(url_for('ingredients'))
--- dietapp/routes.py
+++ dietapp/routes.py
@@ -5,6 +5,7 @@ from flask import render_template, url_for, flash, redirect, request, abort, jso
 from dietapp import app, db, bcrypt
 from dietapp.forms import RegistrationForm, LoginForm, IngredientForm, MealForm, create_MealForm
 from dietapp.models import User, Ingredients, Units, UnitType, Meals, MealIngredients
+from dietapp.route_functions import parse_checkboxes, fcn_save_new_from_form, fcn_update_from_form, create_ingr_list, create_unit_dict, add_ingredients, delete_ingredients
 from flask_login import login_user, current_user, logout_user, login_required
 from collections import namedtuple
 
@@ -16,20 +17,6 @@ def home():
         return redirect(url_for('login'))
 
     return render_template('home.html', title='Home')
-
-
-def parse_checkboxes(form):
-    ingredient_id_list = []
-    for v in form:
-        id = str(v).split("check")[1]
-        #print (str(v) + ": " + str(form[v]) + " - " + id[1])
-        if ("check" in v) and str(form[v]) == 'on':
-            ingredient_id_list.append(id)
-            print (str(v) + ": " + str(form[v]) + " - " + id)
-
-
-    print(ingredient_id_list)
-    return ingredient_id_list
 
 @app.route('/ingredients', methods=['GET','POST'])
 @login_required
@@ -82,43 +69,6 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-def fcn_save_new_from_form(form, save_type):
-    if save_type == 'NewIngredient':
-        ingredient = Ingredients(brand=form.brand.data, name=form.name.data, 
-            fat=form.fat.data, carbs=form.carbs.data, protein=form.protein.data, calories=form.calories.data, 
-            user_id=current_user.id)
-
-        ingredient.serv_weight=form.serv_weight.data
-        ingredient.weight_unit_id=form.drop_weight.data
-        ingredient.serv_volume=form.serv_volume.data
-        ingredient.volume_unit_id=form.drop_volume.data
-        ingredient.serv_count=form.serv_count.data
-        ingredient.count_unit_id=form.drop_count.data
-
-        db.session.add(ingredient)
-
-
-    db.session.commit()
-
-def fcn_update_from_form(form, id, update_type):
-    if update_type == 'UpdateIngredient':
-        ingredient = Ingredients.query.get(id)
-
-        ingredient.brand=form.brand.data
-        ingredient.name=form.name.data
-        ingredient.fat=form.fat.data
-        ingredient.carbs=form.carbs.data
-        ingredient.protein=form.protein.data
-        ingredient.calories=form.calories.data 
-
-        ingredient.serv_weight=form.serv_weight.data
-        ingredient.weight_unit_id=form.drop_weight.data
-        ingredient.serv_volume=form.serv_volume.data
-        ingredient.volume_unit_id=form.drop_volume.data
-        ingredient.serv_count=form.serv_count.data
-        ingredient.count_unit_id=form.drop_count.data
-
-    db.session.commit()
 
 @app.route("/ingredients/new", methods=['GET','POST'])
 @login_required
@@ -180,64 +130,6 @@ def meals():
     return render_template('meals.html',title='Meal', meals=meals)
 
 
-def create_ingr_list(meal):
-    ingr_list = []
-    for i,mi in enumerate(meal.meal_ingredients):
-        serving_unit = Units.query.filter_by(id=mi.unit_id).first()
-
-        if(serving_unit.unit_type_id == 1):
-            ingr_unit = Units.query.filter_by(id=mi.ingredient.weight_unit_id).first()
-            ingr_serving_amount = mi.ingredient.serv_weight
-        elif(serving_unit.unit_type_id == 2):
-            ingr_unit = Units.query.filter_by(id=mi.ingredient.volume_unit_id).first()
-            ingr_serving_amount = mi.ingredient.serv_volume
-        else:
-            ingr_unit = Units.query.filter_by(id=mi.ingredient.count_unit_id).first()
-            ingr_serving_amount = mi.ingredient.serv_count
-
-
-        d = {}
-        d["MealIngrID"] = mi.id
-        d['IngrID'] = mi.ingredient.id
-        d['IngrName'] = mi.ingredient.name
-        d['IngrBrand'] = mi.ingredient.brand
-        d['IngrFat'] = mi.ingredient.fat
-        d['IngrCarbs'] = mi.ingredient.carbs
-        d['IngrProtein'] = mi.ingredient.protein
-        d['IngrCalories'] = mi.ingredient.calories
-        d['IngrWeightAmount'] = mi.ingredient.serv_weight
-        d['IngrVolumeAmount'] = mi.ingredient.serv_volume
-        d['IngrCountAmount'] = mi.ingredient.serv_count
-        d['IngrWeightUnitID'] = mi.ingredient.weight_unit_id
-        d['IngrVolumeUnitID'] = mi.ingredient.volume_unit_id
-        d['IngrCountUnitID'] = mi.ingredient.count_unit_id
-
-        d['ServingUnit'] = serving_unit.name
-        d['ServingAmount'] = mi.serv
-        d['ServingFat'] = mi.ingredient.fat * mi.serv * serving_unit.conversion / (ingr_serving_amount * ingr_unit.conversion)
-        d['ServingCarbs'] = mi.ingredient.carbs * mi.serv * serving_unit.conversion / (ingr_serving_amount * ingr_unit.conversion)
-        d['ServingProtein'] = mi.ingredient.protein * mi.serv * serving_unit.conversion / (ingr_serving_amount * ingr_unit.conversion)
-        d['ServingCalories'] = mi.ingredient.calories * mi.serv * serving_unit.conversion / (ingr_serving_amount * ingr_unit.conversion)
-
-        ingr_list.append(d)
-
-    return ingr_list
-
-def create_unit_dict():
-    units = Units.query.all()
-    unit_dict = {}
-    for i,u in enumerate(units):
-        d = {}
-        d["id"] = u.id
-        d["Name"] = u.name
-        d["Conversion"] = u.conversion
-        d["UnitTypeID"] = u.unit_type_id
-
-        unit_dict[u.id] = d
-
-    return unit_dict
-
-
 @app.route("/meals/<int:meal_id>/update", methods=['GET','POST'])
 @login_required
 def update_meal(meal_id):
@@ -276,39 +168,6 @@ def add_ingredients_to_meal(meal_id):
         return redirect(url_for('update_meal',meal_id=meal_id))
     ingredients = Ingredients.query.filter_by(user_id = current_user.id, obsolete = False).all()
     return render_template('ingredients.html',title='Ingredients', ingredients=ingredients)
-
-@login_required
-def add_ingredients(ingredient_id_list, meal_id):
-    meal = Meals.query.get_or_404(meal_id)
-    if current_user != meal.author:
-        abort(403)
-    for id in ingredient_id_list:
-        ingredient = Ingredients.query.get_or_404(id)
-        if current_user != ingredient.author:
-            abort(403)
-        #TODO: Fix ingredient serving id error
-        if ingredient.weight_unit_id is not None:
-            mi_unit_id = ingredient.weight_unit_id
-        elif ingredient.volume_unit_id is not None:
-            mi_unit_id = ingredient.volume_unit_id
-        elif ingredient.count_unit_id is not None:
-            mi_unit_id = ingredient.count_unit_id
-        mi = MealIngredients(meal_id=meal_id,ingredient_id=id,serv=0,unit_id=mi_unit_id)
-        db.session.add(mi)
-    db.session.commit()
-    #TODO: Remove  return redirect
-    return redirect(url_for('update_meal',meal_id=meal_id))
-
-@login_required
-def delete_ingredients(ingredient_id_list):
-    for id in ingredient_id_list:
-        ingredient = Ingredients.query.get_or_404(id)
-        if current_user != ingredient.author:
-            abort(403)
-        ingredient.obsolete = True
-        db.session.commit()
-    flash('Your ingredient has been deleted', 'success')
-    return redirect(url_for('ingredients'))
 
 
 @app.route("/meals/<int:meal_id>/delete", methods=['POST'])
