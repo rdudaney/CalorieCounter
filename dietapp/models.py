a2990da92e1723962b22cf8f60ee3956a471821b
Rohan Dudaney <rohan.dudaney@gmail.com>
2022-08-20 15:04:29 -0700

    Fixed meal date_eaten model datatype

--- dietapp/models.py
+++ dietapp/models.py
@@ -89,7 +89,7 @@ class MealIngredients(db.Model):
 class Meals(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100), nullable = False)
-    date_eaten = db.Column(db.DateTime)
+    date_eaten = db.Column(db.Date)
     date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow )
     date_used = db.Column(db.DateTime)
     exclude_from_daily = db.Column(db.Boolean, nullable = False, default=False)
